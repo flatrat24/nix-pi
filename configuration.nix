1552e425bb9b54390e9a5a6177f2ef8a8a63f3f3
Ethan Anthony <ethan.anthony@du.edu>
2025-05-17 01:56:56 -0600

    added jellyfin

--- configuration.nix
+++ configuration.nix
@@ -3,50 +3,55 @@
 # https://rbf.dev/blog/2020/05/custom-nixos-build-for-raspberry-pis/#nix-packages-and-image-configuration
 # https://nixos.wiki/wiki/NixOS_on_ARM#Compiling_through_QEMU.2Fkvm
 
-{ pkgs, neovim-config, system, inputs, ... }: {
+{ pkgs, lib, inputs, system, neovim-config, ... }: {
   imports = [
     ./hardware-configuration.nix
   ];
 
-  # allows the use of flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  ##--- Jellyfin ---##
+  services.jellyfin = {
+    enable = true;
+    user = "admin";
+  };
+  environment.systemPackages = with pkgs; [
+    jellyfin
+    jellyfin-web
+    jellyfin-ffmpeg
+  ];
 
+  hardware.enableAllHardware = lib.mkForce false;
+  
   boot = {
     # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
     loader.grub.enable = false;
     # Enables the generation of /boot/extlinux/extlinux.conf
     loader.generic-extlinux-compatible.enable = true;
   };
-
-  # networking config. important for ssh!
+  
   networking = {
     hostName = "pi";
 
     networkmanager.enable = true;
 
-    firewall = {
-      enable = true;
-      allowedTCPPorts = [ ];
-      allowedUDPPorts = [ ];
-    };
+    # firewall = {
+    #   enable = true;
+    #   allowedTCPPorts = [ ];
+    #   allowedUDPPorts = [ ];
+    # };
   };
-
+  
   # the user account on the machine
   users.users.admin = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ];
-    hashedPassword = "$y$j9T$fCvDlybBxH66IEzSdswzQ0$RUdso7st0Z17Jf1vDRGzmImP8Z2KI2N7nV5RPyW1qo.";
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    hashedPassword = "$y$j9T$fCvDlybBxH66IEzSdswzQ0$RUdso7st0Z17Jf1vDRGzmImP8Z2KI2N7nV5RPyW1qo."; # generate with `mkpasswd`
   };
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # I use neovim as my text editor, replace with whatever you like
-  environment.systemPackages = with pkgs; [
-    # neovim
-    wget
-    inputs.neovim-config.defaultPackage
-  ];
+  # allows the use of flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # this allows you to run `nixos-rebuild --target-host admin@this-machine` from
   # a different host. not used in this tutorial, but handy later.
